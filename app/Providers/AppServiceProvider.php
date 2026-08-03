ed1e6ab32a45825cf82763a21cc489152cf19506
Foxream <saidrizkyabizard@gmail.com>
2026-08-03 10:26:59 +0700

    Force HTTPS for production assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Listeners\AssignDefaultRoleOnRegistration;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,13 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        Event::listen(Registered::class, AssignDefaultRoleOnRegistration::class);
+        Event::listen(
+            Registered::class,
+            AssignDefaultRoleOnRegistration::class
+        );
+
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
